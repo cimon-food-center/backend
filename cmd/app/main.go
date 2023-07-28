fdf551255c35e613b5cf3c1f073196a0b4c2406c
ThiccPan <ihilmiderian@gmail.com>
2023-07-28 18:30:49 +0700

    refactor: move main.go

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,15 +1,24 @@
 package main
 
 import (
+	"log"
+
+	"github.com/cimon-food-center/backend/config"
 	"github.com/labstack/echo/v4"
 )
-
 
 func main() {
 	e := echo.New()
 	e.GET("/healthcheck", func(c echo.Context) error {
 		return c.JSON(200, "online")
 	})
+
+	config, err := config.InitConfig()
+	if err != nil {
+		log.Fatal("error starting app:", err)
+	}
+
+	log.Println(*config)
 
 	e.Logger.Fatal(e.Start(":8080"))
 }
